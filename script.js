772981bd3ddd8fd43bccd5bf8633b5106291cd7a
Adam <42845085+adam-weiler@users.noreply.github.com>
2019-02-24 22:31:00 -0500

    Passing 13 of 16 tests.

--- script.js
+++ script.js
@@ -1,133 +1,123 @@
 console.clear();
 
-const url = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
-
+const url = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json"; //Data source.
 
 const svgWidth = 800; //Width of SVG.
 const svgHeight = 500; //Height of SVG.
 const svgPadding = 60; //Padding from edge of SVG.
 
-const timeFormat = d3.timeFormat("%M:%S");
+const timeFormat = d3.timeFormat("%M:%S"); //Y-axis uses "12:34" format for time.
+
+// let colour = d3.scaleOrdinal()
+//       .domain(["Doping", "No Doping"])
+//       .range(["#FF0000", "#0000FF"]);
+
+const createDate = (min,s) => { 
+            let d = new Date();
+            d.setMinutes(min);
+            d.setSeconds(s);
+            return d;
+}
+
 
 
 d3.json(url, function(err, data) { //Gets the data from the json file.
-	//   const justTheYears = data.map(function (item) {
-	//     return item.Year.toString();
-	// });
-  // console.log(justTheYears);
-  
-	// const justTheTime = data.map(function(item) {
-	// 	return item.Time //.replace(/:/g, '')
-	// });
-	// console.log(justTheTime);
+		let xScale = d3.scaleLinear() //Scale for the x-axis.
+		             .domain([d3.min(data, (d) => d.Year) - 1, d3.max(data, (d) => d.Year) + 1]) //Uses smallest and biggest years. Adds 1 year before & after.
+		             .range([svgPadding, svgWidth - svgPadding])
 
-  
-	//Scale for the horizontal x-axis. Finds the smallest year and biggest year. Adds extra year before & after.
-	const xScale = d3.scaleLinear()
-		.domain([d3.min(data, (d) => d["Year"]) - 1, d3.max(data, (d) => d["Year"]) + 1]) 
-		.range([svgPadding, svgWidth - svgPadding]);
-
-	//The vertical y-axis. Finds the longest time and shortest time.
-	const yScale = d3.scaleTime()
-		.domain([d3.max(data, (d) => d["Seconds"]), d3.min(data, (d) => d["Seconds"])])
-		.range([svgHeight - svgPadding, svgPadding]);
-  
-  //The horzintal x-axis for years. Uses ticks.
-  	const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
-
-   //The vertical y-axis for time.
-	//const yAxis = d3.axisLeft(yScale)//.ticks(d3.time.seconds, 15).tickFormat(d3.time.format("%M:%S"));
-
+		let xAxis = d3.axisBottom(xScale) //The x-axis.
+                .tickFormat(d3.format("d")) //Formats years as "1994" instead of "1,994".
   
   
-  var yAxis = d3.axisLeft(yScale).tickFormat(timeFormat);
+  
+		let milliseconds = data.map(function(d, i){ //An array of data from Seconds.
+      	return data[i].Seconds * 1000; //Multiplied by 1000 to convert to miliseconds.
+		});
+    
+  	let minutes = data.map(function(d,i){ //An array of data from Time.
+				return data[i].Time
+  	});
+   
+		const yScale = d3.scaleTime() //Scale for the y-axis.
+		//							 .domain([d3.max(data, (d) => d["Seconds"]), d3.min(data, (d) => d["Seconds"])])
+  									 .domain([d3.max(milliseconds), d3.min(milliseconds)]) //Uses longest and shortest times from the seconds array.
+										 .range([svgHeight - svgPadding, svgPadding]);
+  
+		//The vertical y-axis for time.
+		//const yAxis = d3.axisLeft(yScale)//.ticks(d3.time.seconds, 15).tickFormat(d3.time.format("%M:%S"));
+		//var yAxis = d3.axisLeft(yScale).tickFormat(timeFormat);
+
+		let yAxis = d3.axisLeft(yScale) //The y-axis.
+                  .tickFormat(timeFormat); //Y-axis uses "12:34" format for time.
+
   
   
-  //This is the main SVG file.
-	const svg = d3.select("body")
-		.append("svg")
-		.attr("width", svgWidth)
-		.attr("height", svgHeight);
+		const svgContainer = d3.select("body") //This is the main SVG element.
+		                     	 .append("svg")
+		                       .attr("width", svgWidth)
+	                         .attr("height", svgHeight);
 
+		svgContainer.selectAll("circle") //These are the scatterplot dots.
+								.data(data)
+								.enter()
+								.append("circle")
+	// 						.attr("class", "dot hoverDot")
+		 						.attr("cx", (d) => xScale(d["Year"])) //Controls x-value for dots. Uses Year from data.
+	//						.attr("cy", (d) => yScale(d["Seconds"]))
+								.attr('cy', function(d,i){ return yScale(milliseconds[i]) }) //Controls y-value for dots. Uses milliseconds array.
+  							.attr("r", (d) => 5) //Radius of dots.
+								.attr("data-xvalue", (d) => d["Year"]) //X-value only visible in chrome dev tools.
+		//					.attr("data-yvalue", (d) => new Date(d["Seconds"] * 1000)) //Yvalue visible in chrome dev tools.
+                .attr('data-yvalue', (d) => createDate( Number(d.Time.split(':')[0]), Number(d.Time.split(':')[1]) )) //Yvalue visible in chrome dev tools.
+                .attr("class", (d) => {
+		                return (d["Doping"]) ? "dot allgedDoping" : "dot noDoping";
+		            });
 
-	//These are the scatterplot dots.
-	svg.selectAll("circle")
-		.data(data)
-		.enter()
-		.append("circle")
-		.attr("class", "dot")
-		.attr("cx", (d) => xScale(d["Year"]))
-		.attr("cy", (d) => yScale(d["Seconds"]))
-		.attr("r", (d) => 5)
-		.attr("data-xvalue", (d) => d["Year"]) //Xvalue visible in chrome dev tools.
-		.attr("data-yvalue", (d) => new Date(d["Seconds"] * 1000)) //Yvalue visible in chrome dev tools.
+	// svg.selectAll("text")
+	//  .data(data)
+	//  .enter()
+	//  .append("text")
+	//  .text((d) =>  (d[0] + "," + d[1]))
+	//  .attr("x", (d) => xScale(d[0] + 10))
+	//  .attr("y", (d) => yScale(d[1]))
 
+		svgContainer.append("g") //Appends the x-axis to the SVG file.
+								.attr("id", "x-axis")
+								.attr("transform", "translate(0," + (svgHeight - svgPadding) + ")")
+								.call(xAxis)
 
+		svgContainer.append("g") //Appends the y-axis to the SVG file.
+  	   					.call(yAxis)
+		 						.attr("id", "y-axis")
+		 						.attr("transform", "translate(" + (svgPadding) + ",0)")
 
-	// Format data for Y Axis
-	//        var specifier = "%M:%S";
-	//        var parsedTime = justTheTime.map(function(d) {
-	//          return d3.timeParse(specifier)(d)
-	//        })
+  
+	var legend = svgContainer.selectAll(".legend") //This is the Legend element.
+		              				 .data("YN")
+		              				 .enter().append("g")
+	              					 .attr("class", "legend")
+		              				 .attr("transform", function(d, i) {
+			                        return "translate(0," + i * 20 + ")";
+													 });
 
-	//        var y = d3.scaleTime()
-	//                  .range([svgHeight, 0])
-	//                  .domain(d3.extent(parsedTime));
+	legend.append("rect") //Appends the rectangles to the Legend.
+		    .attr("x", svgWidth - 18)
+		    .attr("width", 18)
+		    .attr("height", 18)
+		    .style("fill", "red");
 
-	//   //Also Y-Axis
-	//  svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-	//          g.append("g")
-	//         .call(d3.axisLeft(y)
-	//              .tickFormat(function(d,i) {
-	//     return times[i]
-	// }))
-	//         .attr("stroke-width", 2)
-	//            .style("font-size", ".6em")
+	legend.append("text") //Appends the text to the Legend.
+		    .attr("x", svgWidth - 24)
+		    .attr("y", 9)
+		    .attr("dy", ".35em")
+		    .style("text-anchor", "end")
+		    .text(function(d) {
+			      return d;
+		    })
 
-
-
-// 	svg.selectAll("text")
-// 	 .data(data)
-// 	 .enter()
-// 	 .append("text")
-// 	 .text((d) =>  (d[0] + "," + d[1]))
-// 	 .attr("x", (d) => xScale(d[0] + 10))
-// 	 .attr("y", (d) => yScale(d[1]))
-
-
-
-	//The horizontal x-axis.
-	svg.append("g")
-		.attr("id", "x-axis")
-		.attr("transform", "translate(0," + (svgHeight - svgPadding) + ")")
-		.call(xAxis)
-
-
-//The vertical y-axis.
-	svg.append("g")
-		.attr("id", "y-axis")
-		.attr("transform", "translate(" + (svgPadding) + ",0)")
-		.call(yAxis)
-
-
-
-	//   legend = svg.append("g")
-	//    .attr("class","legend")
-	//    .attr("transform","translate(50,30)")
-	//    .style("font-size","12px")
-
-	//   .selectAll('circle')
-	//       .data(data)
-	//       .enter()
-	//   // .call(d3.legend)
-
-
-	console.log(data[0])
-	console.log("Time", data[0]["Time"])
-	console.log("justTheTime", justTheTime[0])
-
-
-
+	//console.log(data[0])
+	//console.log("Time", data[0]["Time"])
 })
 
-console.log("end of app")
+console.log("end of app");
